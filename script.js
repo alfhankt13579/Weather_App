01fb22bbd31a4dede5429e30c17bbd7f84480d31
alfhankt13579 <alfhankt13579@gmail.com>
2023-07-16 02:19:24 +0530

    Weather App

---
+++ script.js
@@ -0,0 +1,22 @@
+function city() {
+  let cities = document.getElementById("search").value;
+
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cities}&appid=68b0b5f0db57621f8d1e51cddcf907aa&units=metric`
+  )
+    .then((result) => result.json())
+    .then((data) => weatherData(data))
+    .catch((error) => alert("failed to fetch"));
+}
+
+function weatherData(data) {
+  api2.innerHTML = `<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">`;
+  api3.innerHTML = `${data.weather[0].description}`;
+  api4.innerHTML = `${data.main.temp.toFixed(1)}°C`;
+  api5.innerHTML = `Feels like ${data.main.feels_like.toFixed(1)}°C`;
+  api6.innerHTML = `${data.main.pressure}hPa`;
+  api7.innerHTML = `${data.main.humidity}%`;
+  api8.innerHTML = `${data.wind.speed.toFixed(1)}m/s`;
+  api9.innerHTML = `${(data.visibility / 1000).toFixed(1)}km`;
+  api1.innerHTML = ``;
+}
