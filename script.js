2d765f54eeafb073d4e97a615d24704657bbabdb
alfhankt13579 <alfhankt13579@gmail.com>
2023-09-18 03:30:53 +0530

    Weather App

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ function city() {
   )
     .then((result) => result.json())
     .then((data) => weatherData(data))
-    .catch((error) => alert("failed to fetch"));
+    .catch(() => alert("Invalid Input"));
 }
 
 function weatherData(data) {
   api2.innerHTML = `<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">`;
-  api3.innerHTML = `${data.weather[0].description}`;
-  api4.innerHTML = `${data.main.temp.toFixed(1)}°C`;
-  api5.innerHTML = `Feels like ${data.main.feels_like.toFixed(1)}°C`;
+  api1.innerHTML = ``;
+  api3.innerHTML = `Feels like ${data.main.feels_like.toFixed(1)}°C`;
+  api4.innerHTML = `${data.name}, ${data.main.temp.toFixed(1)}°C`;
+  api5.innerHTML = `${data.weather[0].description}`;
   api6.innerHTML = `${data.main.pressure}hPa`;
   api7.innerHTML = `${data.main.humidity}%`;
   api8.innerHTML = `${data.wind.speed.toFixed(1)}m/s`;
   api9.innerHTML = `${(data.visibility / 1000).toFixed(1)}km`;
-  api1.innerHTML = ``;
 }
